92953d2e5a99b805099132f82a4c52f143b517e5
Doug Wollison <doug@wollison.net>
2015-04-28 11:30:48 -0400

    Figured out how to make setup_callback() work.

--- class-smart-plugin.php
+++ class-smart-plugin.php
@@ -142,11 +142,14 @@ abstract class Smart_Plugin{
 	 */
 	public function __call( $method, $args ) {
 		/**
-		 * Check if the method name is a real method,
+		 * Check if the method name is setup_callback,
+		 * or a real method,
 		 * or a callback alias.
-		 * Abort if neither.
+		 * Abort if none of the above.
 		 */
-		if ( method_exists( $this, "_$method" ) ) {
+		if ( $method == 'setup_callback' ) {
+			return call_user_func_array( array( $this, 'save_callback' ), $args );
+		} elseif ( method_exists( $this, "_$method" ) ) {
 			return $this->save_callback( $method, $args );
 		} elseif ( preg_match( '/^cb(\d+)/', $method, $matches ) ) {
 			return $this->load_callback( $matches[1], $args );
@@ -200,11 +203,14 @@ abstract class Smart_Plugin{
 	 */
 	public static function __callStatic( $method, $args ) {
 		/**
-		 * Check if the method name is a real method,
+		 * Check if the method name is setup_callback,
+		 * or a real method,
 		 * or a callback alias.
-		 * Abort if neither.
+		 * Abort if none of the above.
 		 */
-		if ( method_exists( get_called_class(), "_$method" ) ) {
+		if ( $method == 'setup_callback' ) {
+			return call_user_func_array( array( get_called_class(), 'save_static_callback' ), $args );
+		} elseif ( method_exists( get_called_class(), "_$method" ) ) {
 			return static::save_static_callback( $method, $args );
 		} elseif ( preg_match( '/^cb(\d+)/', $method, $matches ) ) {
 			return static::load_static_callback( $matches[1], $args );
